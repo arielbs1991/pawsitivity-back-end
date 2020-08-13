4216ce4323e4108073cd0cbb0ac030c1cb127c9a
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-12 20:32:12 -0700

    finalized a lot of tweaks. You can now search by type,breed, and location. Returns up to 100 results

--- utils/petAPI/API.js
+++ utils/petAPI/API.js
@@ -1,31 +1,42 @@
 import axios from 'axios';
 
-const petAPI = () => {
-  const BASEURL = 'https://api.petfinder.com/v2/animals?type=dog&breed='
+const petAPI = (type, breed, location) => {
+
+  const BASEURL = `https://api.petfinder.com/v2/animals?type=${type}&breed=${breed}&limit=100&location=${location}`
   const publicKey = 'yaD3Y8GufBtJCkntjc4byTSBHVYUPeD42PJAZq3GO2SfRx8p9g'
   const secretKey = 'TLsMatYIB2EQQhUzmkkbPY1M2m0KljgiXqInort4'
-  
+
   const getToken = async () => {
-    const config = {
-      url: "oauth2/token",
-      method: "post",
-      baseURL: "https://api.petfinder.com/v2/",
-      data: {
-        grant_type: "client_credentials",
-        client_id: `${publicKey}`,
-        client_secret: `${secretKey}`,
-      },
-    };
-    let res = await axios(config);
-    return res.data.access_token
+    try {
+      const config = {
+        url: "oauth2/token",
+        method: "post",
+        baseURL: "https://api.petfinder.com/v2/",
+        data: {
+          grant_type: "client_credentials",
+          client_id: `${publicKey}`,
+          client_secret: `${secretKey}`,
+        },
+      };
+
+      let res = await axios(config);
+      return res.data.access_token
+
+
+    } catch (err) {
+      console.error(err)
+    }
   }
-  const queryPetfinder = async (token, breed) => {
-    return await axios.get(BASEURL + breed, { headers: { "Authorization": `Bearer ${token}` } })
+
+  const queryPetfinder = async (token) => {
+    try {
+      return await axios.get(BASEURL, { headers: { "Authorization": `Bearer ${token}` } })
+    } catch (err) {
+      console.error(err)
+    }
   }
-  
-  getToken().then(token => queryPetfinder(token, "retriever").then(res => {
-    return res.data.animals
-  }))
+
+  return getToken().then(token => queryPetfinder(token).catch(err => console.log(err)))
 }
 
 export default petAPI;

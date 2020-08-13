7f9a9cb970a8bf05256aec324f6ca3a133d23f47
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-12 21:10:51 -0700

    thanks denis for the code improvements

--- utils/petAPI/API.js
+++ utils/petAPI/API.js
@@ -1,42 +1,23 @@
-import axios from 'axios';
+const axios = require('axios');
 
-const petAPI = (type, breed, location) => {
-
+const petAPI = async (type, breed, location) => {
   const BASEURL = `https://api.petfinder.com/v2/animals?type=${type}&breed=${breed}&limit=100&location=${location}`
   const publicKey = 'yaD3Y8GufBtJCkntjc4byTSBHVYUPeD42PJAZq3GO2SfRx8p9g'
   const secretKey = 'TLsMatYIB2EQQhUzmkkbPY1M2m0KljgiXqInort4'
-
-  const getToken = async () => {
-    try {
-      const config = {
-        url: "oauth2/token",
-        method: "post",
-        baseURL: "https://api.petfinder.com/v2/",
-        data: {
-          grant_type: "client_credentials",
-          client_id: `${publicKey}`,
-          client_secret: `${secretKey}`,
-        },
-      };
-
-      let res = await axios(config);
-      return res.data.access_token
-
-
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  const queryPetfinder = async (token) => {
-    try {
-      return await axios.get(BASEURL, { headers: { "Authorization": `Bearer ${token}` } })
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  return getToken().then(token => queryPetfinder(token).catch(err => console.log(err)))
+  const config = {
+    url: "oauth2/token",
+    method: "post",
+    baseURL: "https://api.petfinder.com/v2/",
+    data: {
+      grant_type: "client_credentials",
+      client_id: `${publicKey}`,
+      client_secret: `${secretKey}`,
+    },
+  };
+  
+  let { data: { access_token } } = await axios(config);
+  const { data: { animals } } = await axios.get(BASEURL, { headers: { "Authorization": `Bearer ${access_token}` } })
+  return animals
 }
 
-export default petAPI;
+module.exports = petAPI;

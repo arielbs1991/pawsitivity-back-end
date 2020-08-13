6dda91f0736cf324a36c7f388368a1918f036957
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-13 15:47:04 -0700

    made a route for the petfinder api so that front end could hit it up without exposing the keys.

--- controllers/petAPIController.js
+++ controllers/petAPIController.js
@@ -1,6 +1,16 @@
-// const router = require("express").Router();
-// const petAPI = require("../utils/petAPI/API.js");
-// const db = require("../models");
+const router = require("express").Router();
+const petAPI = require("../utils/petAPI/API.js");
+const db = require("../models");
 
 //I WAS TRYING TO GET USER INFORMATION / PREFERENCES BY ID AND THEN RUN A PETAPI QUERY BASED ON THOSE AND THEN TURN THAT INTO A JSON OBJECT BUT IDEFK
 //STORE USER OBJECT IN SESSIONS THEN WE DON'T HAVE TO FIND IT
+
+router.get("/api/pets/", ({ body: { type, location } }, res) => {
+    petAPI(type, location)
+        .then(petResults => {
+
+            res.json(petResults)
+        })
+})
+
+module.exports = router

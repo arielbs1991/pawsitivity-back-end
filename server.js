3738c953bfb00d4777a5fba388ab56f5fc337194
Ariel <arielbs1991@gmail.com>
2020-08-14 11:35:56 -0700

    added cors, preparing to push

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const db = require("./models");
 var PORT = process.env.PORT || 3001;
 const morgan = require("morgan"); //added for mail
 const nodemailer = require("nodemailer"); //added for mail
+const cors = require("cors"); //added for cors
 require('dotenv').config(); //added for mail
 app.use('/sendEmail', require('./routes/sendEmail')) //added for mail
 
@@ -12,6 +13,25 @@ app.use(express.json());
 
 app.use(morgan('dev')) //added for mail
 
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true
+}))
+
+//this one Cody sent me for cors
+// app.use(session({
+//   secret: "keyboard cat",
+//   resave: false,
+//   saveUninitialized: false,
+//   proxy: true,
+//   cookie: {
+//     maxAge: 2 * 60 * 60 * 1000,
+//     sameSite: "none",
+//     secure: true
+//   }
+// }))
+
+//this one is a holdover from login process from winekey
 // app.use(session({
 //   secret: "keyboard cat",
 //   resave: false,
@@ -36,8 +56,8 @@ app.use("/api/petAPI", petAPIController);
 
 
 //TODO: once our db is where we want it, change to force:false
-db.sequelize.sync({ force:false }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });

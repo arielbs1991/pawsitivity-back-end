83ce2be3d829bab2a04e895975e8fcffb79597e6
Ariel <arielbs1991@gmail.com>
2020-08-15 11:16:37 -0700

    checking out my twilio branch to put all the variable names back

--- controllers/petAPIController.js
+++ controllers/petAPIController.js
@@ -1,6 +1,41 @@
 const router = require("express").Router();
-const petAPI = require("../utils/petAPI/API.js");
+const petAPIbyUserPref = require("../utils/petAPIbyUserPref/API.js");
 const db = require("../models");
+const shelterAPI = require("../utils/shelterAPI/API");
+
+//BASE URL FOR ALL ROUTES ON THIS PAGE: /api/petAPI
+
+
+//route for storing shelter info in shelter_table
+router.post('/shelter', (req, res) => {
+    db.Shelter.create({
+        shelterName: req.body.shelterName,
+        orgId: req.body.orgId,
+        email: req.body.email,
+        address: req.body.address,
+        phoneNumber: req.body.phoneNumber
+    })
+        .then(shelterData => {
+            res.json(shelterData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).end()
+        })
+})
+
+//route to find shelter data by id
+router.get('/shelter/:id', (req, res) => {
+    id = req.body.orgId;
+    shelterAPI(id)
+        .then(shelterResults => {
+            res.json(shelterResults)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).end()
+        })
+})
 
 //I WAS TRYING TO GET USER INFORMATION / PREFERENCES BY ID AND THEN RUN A PETAPI QUERY BASED ON THOSE AND THEN TURN THAT INTO A JSON OBJECT BUT IDEFK
 //STORE USER OBJECT IN SESSIONS THEN WE DON'T HAVE TO FIND IT
@@ -15,6 +50,17 @@ const db = require("../models");
 
 
 router.get("/pets/", ({ body: { type, location, hasKids, hasCats, hasDogs } }, res) => {
+    petAPIbyUserPref(type, location, hasKids, hasCats, hasDogs)
+        .then(petResults => {
+            res.json(petResults)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).end()
+        })
+})
+
+router.post("/pets/", ({ body: { type, location, hasKids, hasCats, hasDogs } }, res) => {
     petAPIbyUserPref(type, location, hasKids, hasCats, hasDogs)
         .then(petResults => {
             res.json(petResults)

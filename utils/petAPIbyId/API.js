a4b305d4d40df1577a0c4429832c3cb6ae1f09f6
Cody Samuels <twowoodenspoons@gmail.com>
2020-08-20 17:07:06 -0700

    even more bug fixes.

--- utils/petAPIbyId/API.js
+++ utils/petAPIbyId/API.js
@@ -1,22 +1,10 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const petAPIbyId = async (petId) => {
+const petAPIbyId = async (petId, token) => {
   const BASEURL = `https://api.petfinder.com/v2/animals/${petId}`
-  const publicKey = 'Id4PWiv8ndIIegWRHTHLuj9sxhelO9TS0G2A0UNKbSDpm6117V'
-  const secretKey = process.env.PETFINDER_API
-  const config = {
-    url: "oauth2/token",
-    method: "post",
-    baseURL: "https://api.petfinder.com/v2/",
-    data: {
-      grant_type: "client_credentials",
-      client_id: `${publicKey}`,
-      client_secret: `${secretKey}`,
-    },
-  };
-  let { data: { access_token } } = await axios(config);
-  const results = await axios.get(BASEURL, { headers: { "Authorization": `Bearer ${access_token}` } })
+
+  const results = await axios.get(BASEURL, { headers: { "Authorization": `Bearer ${token}` } })
   return results.data
 }
 module.exports = petAPIbyId;

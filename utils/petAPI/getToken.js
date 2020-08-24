6de53bed9d07d14c092a223d39f5f0a5f05dcc37
Ariel <arielbs1991@gmail.com>
2020-08-24 12:44:04 -0700

    changed public keys (currently on Ariel's)

--- utils/petAPI/getToken.js
+++ utils/petAPI/getToken.js
@@ -2,8 +2,10 @@ const axios = require('axios');
 require('dotenv').config();
 
 const getToken = () => {
-  // const publicKey = 'Id4PWiv8ndIIegWRHTHLuj9sxhelO9TS0G2A0UNKbSDpm6117V'
-  const publicKey = 'yaD3Y8GufBtJCkntjc4byTSBHVYUPeD42PJAZq3GO2SfRx8p9g'
+  //Ariel Key
+  const publicKey = 'Id4PWiv8ndIIegWRHTHLuj9sxhelO9TS0G2A0UNKbSDpm6117V'
+  //Cody Key
+  // const publicKey = 'yaD3Y8GufBtJCkntjc4byTSBHVYUPeD42PJAZq3GO2SfRx8p9g'
   const secretKey = process.env.PETFINDER_API
   const config = {
     url: "oauth2/token",

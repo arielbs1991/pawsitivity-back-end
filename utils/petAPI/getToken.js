b14d693e4bdc7158be63280726c64043015e3c69
Ariel <arielbs1991@gmail.com>
2020-08-25 08:43:47 -0700

    changed petfinder key to Cody's

--- utils/petAPI/getToken.js
+++ utils/petAPI/getToken.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const getToken = () => {
   //Ariel Key
-  const publicKey = 'Id4PWiv8ndIIegWRHTHLuj9sxhelO9TS0G2A0UNKbSDpm6117V'
+  // const publicKey = 'Id4PWiv8ndIIegWRHTHLuj9sxhelO9TS0G2A0UNKbSDpm6117V'
   //Cody Key
-  // const publicKey = 'yaD3Y8GufBtJCkntjc4byTSBHVYUPeD42PJAZq3GO2SfRx8p9g'
+  const publicKey = 'yaD3Y8GufBtJCkntjc4byTSBHVYUPeD42PJAZq3GO2SfRx8p9g'
   const secretKey = process.env.PETFINDER_API
   const config = {
     url: "oauth2/token",
